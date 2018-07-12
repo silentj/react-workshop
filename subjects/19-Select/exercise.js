32fda2fd87b3bb234497a46d9feea9cc53a41cfa
Brent Plump <bplump@atlassian.com>
2018-07-12 13:05:40 +1000

    Implemented the Select exercise

--- subjects/19-Select/exercise.js
+++ subjects/19-Select/exercise.js
@@ -16,21 +16,62 @@ class Select extends React.Component {
     defaultValue: PropTypes.any
   };
 
+  state = {
+    value: this.props.defaultValue,
+    isOpen: false,
+  };
+
+  isControlled() {
+    return this.props.value != null;
+  }
+
+  getValue() {
+    return this.isControlled() ? this.props.value : this.state.value;
+  }
+
+  handleLabelClick = (e, newValue) => {
+    this.setState(prevState => {
+      return {isOpen: !prevState.isOpen}
+    })
+  };
+
+  handleChildClick = newValue => {
+    const newState = this.isControlled() ?
+      {
+        isOpen: false,
+      } :
+      {
+        isOpen: false,
+        value: newValue,
+      };
+    this.setState(newState);
+    if (this.props.onChange) {
+      this.props.onChange(newValue);
+    }
+  };
+
   render() {
+    const selectedChild = React.Children.toArray(this.props.children).find(child => child.props.value === this.getValue());
     return (
       <div className="select">
-        <div className="label">
-          label <span className="arrow">▾</span>
+        <div className="label" onClick={this.handleLabelClick}>
+          {selectedChild.props.children} <span className="arrow">▾</span>
         </div>
-        <div className="options">{this.props.children}</div>
+        {this.state.isOpen ? <div className="options">{React.Children.map(this.props.children, child =>
+          React.cloneElement(child, {_onClick: () => this.handleChildClick(child.props.value)}))
+        }</div> : null}
       </div>
     );
   }
 }
 
 class Option extends React.Component {
+  static propTypes = {
+    _onClick: PropTypes.func,
+  };
+
   render() {
-    return <div className="option">{this.props.children}</div>;
+    return <div className="option" onClick={this.props._onClick}>{this.props.children}</div>;
   }
 }
 
